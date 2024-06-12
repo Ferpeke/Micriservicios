15d42f1c4b2d50123278303853b0af4a9ae3da7d
Fernando Sebastian <ferna9752@gmail.com>
2024-06-12 12:08:55 -0600

    Ultimos movimientos

--- Gen6-Infraestructure2024/Gen6-EurekaServer2024/src/main/java/com/sebastian/eureka/Gen6EurekaServer2024Application.java
+++ Gen6-Infraestructure2024/Gen6-EurekaServer2024/src/main/java/com/sebastian/eureka/Gen6EurekaServer2024Application.java
@@ -2,8 +2,10 @@ package com.sebastian.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Gen6EurekaServer2024Application {
 
 	public static void main(String[] args) {
