d712bbe7087b6ad3ab08a45bb96d1b4c96cd5eab
Fernando Sebastian <ferna9752@gmail.com>
2024-06-12 11:41:51 -0600

    Modifcacion del proyecto Eureka

---
+++ Gen6-Infraestructure2024/Gen6-EurekaServer2024/src/main/java/com/sebastian/eureka/Gen6EurekaServer2024Application.java
@@ -0,0 +1,13 @@
+package com.sebastian.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gen6EurekaServer2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gen6EurekaServer2024Application.class, args);
+	}
+
+}
---
+++ Gen6-Infraestructure2024/Gen6-EurekaServer2024/src/test/java/com/sebastian/eureka/Gen6EurekaServer2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gen6EurekaServer2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
